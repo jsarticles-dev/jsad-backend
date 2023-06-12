6dd5cda949f5ead9dd36d96c200f2055096c70f1
Ertan Özdemir <ertan_kubi@hotmail.com>
2023-06-12 11:41:43 +0300

    Do cors configs

---
+++ src/config/cors.ts
@@ -0,0 +1,31 @@
+import { CorsOptions } from "cors";
+import { ENVIRONMENTS } from "../constants/environmentConstants";
+
+const getCorsOptions = (): CorsOptions => {
+  let whitelist: string[] = [];
+
+  if (process.env.NODE_ENV === ENVIRONMENTS.DEVELOPMENT) {
+    whitelist = ["http://localhost:3000/"];
+  }
+
+  if (process.env.NODE_ENV === ENVIRONMENTS.PRODUCTION) {
+    whitelist = ["https://jsartciles.dev/"];
+  }
+
+  if (whitelist.length === 0) {
+    console.log("Whitelist cannot be empty!");
+    throw new Error("Something went wrong");
+  }
+
+  return {
+    origin: function (origin, callback) {
+      if (origin && whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
+};
+
+export default getCorsOptions;
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import express, { Express } from "express";
 import "dotenv/config";
 import userRouter from "./routes/user";
 import connectToDB from "./config/database";
-
+import cors from "cors";
+import getCorsOptions from "./config/cors";
 const app: Express = express();
+
+app.use(cors(getCorsOptions()));
 
 app.use("/user", userRouter);
 
