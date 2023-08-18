5261608fd7bd5fb28b9c5dcda883985005e08288
Ertan Özdemir <ertan_kubi@hotmail.com>
2023-08-18 15:15:44 +0300

    Remove redundant codes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import express, { Express } from "express";
 import "dotenv/config";
 import userRouter from "./routes/user";
-import { getConnectionString } from "./configs/database";
 import cors from "cors";
 //import getCorsOptions from "./configs/cors";
 import helmet from "helmet";
-import session from "express-session";
-import MongoStore from "connect-mongo";
 import employeeRouter from "./routes/employee";
 import newsletterRouter from "./routes/newsletter";
 import authRouter from "./routes/auth";
@@ -15,18 +12,7 @@ const app: Express = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET as string,
-    store: MongoStore.create({ mongoUrl: getConnectionString() }),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: true,
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    },
-  })
-);
+
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
